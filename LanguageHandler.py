cc8d362d7152f995985955717a8fec6ef6242ca7
Max DeVos <devosmaxwell@gmail.com>
2021-02-18 00:32:08 -0600

    Added formatted console logs

--- LanguageHandler.py
+++ LanguageHandler.py
@@ -1,13 +1,15 @@
 import langdetect
 from googletrans import Translator
+import time
 translator = Translator()
 
 async def determine_language(message):
-    # try:
-    lang = translator.detect(message.content)
-    print(lang)
-    if lang.lang == "es" or lang.lang == "de":
-        print(message.author.name, ":", lang)
-        translation = translator.translate(message.content)
-        await message.reply("**Translation:  **"+translation.text, mention_author=False)
-# except Exception as e:
+    try:
+        lang = translator.detect(message.content)
+        # print("[LANGUAGE] ", lang)
+        if lang.lang == "es" or lang.lang == "de":
+            print("[LANGUAGE] ["+time.strftime('%Y-%m-%d %H:%M:%S')+"]", message.author.name+":", lang.lang)
+            translation = translator.translate(message.content)
+            await message.reply("**Translation:  **"+translation.text, mention_author=False)
+    except Exception as e:
+        print("[LANGUAGE] Failed:", e)
